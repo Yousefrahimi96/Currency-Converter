38372784b344d24505b92aef0f85e130909a7a13
Yousefrahimi96 <yousefrahimi310@gmail.com>
2025-05-04 17:06:15 +0330

    pep8 standard was applied

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ import streamlit as st
 from src.currency import CURRENCIES
 from src.functions import get_exchange_rate, convert_currency
 
+st.title('Currency Converter')
 col1, col2 = st.columns(2)
 
 with col1:
+    st.subheader('Exchange Rate')
     base_currency = st.selectbox('Base Currency', CURRENCIES)
     target_currency = st.selectbox('Target Currency', CURRENCIES)
 
-    target_value = get_exchange_rate(base_currency, target_currency)
-    st.write(target_value, target_currency)
+    exchange_rate = get_exchange_rate(base_currency, target_currency)
+    st.write('1', base_currency, '=', exchange_rate, target_currency)
 
 with col2:
-    currency = st.selectbox('Currency', CURRENCIES)
-    mount = st.number_input('Mount')
-    finall_value = convert_currency(currency, mount)
-    st.write(finall_value, 'USD')
+    st.subheader('Convert to USD')
+    from_currency = st.selectbox('from_currency', CURRENCIES)
+    amount = st.number_input('Amount')
+    final_value = convert_currency(from_currency, amount)
+    st.write(amount, from_currency, '=', final_value, 'USD')
